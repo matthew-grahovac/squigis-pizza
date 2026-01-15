a2a85aa84e986033e6fde797d9ed16ed98b4950e
kismattic <kismattic@outlook.com>
2026-01-14 20:28:53 -0600

    adding basic updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://matthew-grahovac.github.io',
+    base: '/squigis-pizza',
+    vite: {    
+        plugins: [tailwindcss()],  
+    },
+});
